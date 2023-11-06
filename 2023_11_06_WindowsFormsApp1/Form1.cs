cd0cfa3e0687b0bcc83fdc92f94cc280f3be45dc
baedole <qodydwls1013@gmail.com>
2023-11-06 17:16:04 +0900

    first commit test

--- 2023_11_06_WindowsFormsApp1/Form1.cs
+++ 2023_11_06_WindowsFormsApp1/Form1.cs
@@ -17,6 +17,9 @@ namespace _2023_11_06_WindowsFormsApp1
             InitializeComponent();
             //MessageBox.Show("안녕하세요우?");
             textBox1_print.Text = "이것은\r\n멀티라인\r\n텍스트박스\r\n인데요.";
+            int a = 0;
+            // stage
+            // not stagess
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
