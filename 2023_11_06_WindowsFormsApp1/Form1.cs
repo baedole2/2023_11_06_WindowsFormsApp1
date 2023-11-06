4273145a21a52dd27413d3692934f3fec85b0bac
baedole <qodydwls1013@gmail.com>
2023-11-06 16:57:30 +0900

    Add project files.

---
+++ 2023_11_06_WindowsFormsApp1/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2023_11_06_WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //MessageBox.Show("안녕하세요우?");
+            textBox1_print.Text = "이것은\r\n멀티라인\r\n텍스트박스\r\n인데요.";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
